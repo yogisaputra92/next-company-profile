f436947c194f64709ade77ea9e968e303ae9b099
yogisaputra92 <yogisaputra.ys92@gmail.com>
2025-02-28 23:18:49 +0700

    feat: menambahkan animasi SplashCursor

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Services from "./components/Services";
+import SplashCursor from "@/components/Animations/SplashCursor/SplashCursor";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Navbar/>
       <Header/>
       <Services/>
+      <SplashCursor/>
     </div>
   );
 }
